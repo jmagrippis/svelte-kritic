330845a5d3a739835ffff0ce56e9480f80d6083b
Johnny Magrippis <j@magrippis.com>
2023-12-06 11:12:18 +0100

    ✨  More interesting about page

---
+++ src/lib/components/OutboundLink.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import YouTube from '$lib/icons/YouTube.svelte'
+
+	export let href: string
+</script>
+
+<a
+	{href}
+	class="transition-filter text-foreground-accent underline duration-300 ease-out hover:brightness-150"
+	target="_blank"
+	rel="noopener noreferrer"><slot /></a
+>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,28 @@
-<main class="flex grow flex-col items-center justify-center">
-	This app is meant to accompany a video by Johnny!
+<script>
+	import OutboundLink from '$lib/components/OutboundLink.svelte'
+	import YouTubeLink from '$lib/components/YouTubeLink.svelte'
+</script>
+
+<main
+	class="content-grid mb-4 gap-y-4 pt-[5vh] text-xl lg:mb-8 lg:gap-y-8 lg:pt-[10vh] lg:text-2xl"
+>
+	<h1 class="text-4xl font-bold lg:text-6xl">About</h1>
+	<p>
+		SvelteKritic is the <OutboundLink href="https://kit.svelte.dev/"
+			>SvelteKit</OutboundLink
+		>
+		way to query the
+		<OutboundLink href="https://opencritic.com/">OpenCritic API</OutboundLink>!
+		See the latest and greatest games, without intrusive adds, while still
+		supporting the great OpenCritic project 😄
+	</p>
+	<p>
+		Above all, SvelteKritic is a demo app meant to accompany Johnny’s videos on
+		the <YouTubeLink href="https://www.youtube.com/@johnnifytech"
+			>View Transitions API</YouTubeLink
+		>! Give your web apps that
+		<YouTubeLink href="https://www.youtube.com/@johnnifytech"
+			>App-Like feel</YouTubeLink
+		> 🚀, and delight your users!
+	</p>
 </main>
