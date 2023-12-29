a810cfe86a0b89e2c84673173fbdc579857ab855
Johnny Magrippis <j@magrippis.com>
2023-12-29 16:28:35 +0100

    ✨  group games to tiers

--- src/routes/tierlists/2023/+page.server.ts
+++ src/routes/tierlists/2023/+page.server.ts
@@ -1,9 +1,32 @@
+import type {TierListGame} from '$lib/server/repos/tierLists/TierListsInterface'
 import type {PageServerLoad} from './$types'
 
 export const load: PageServerLoad = async ({locals}) => {
 	const games = await locals.tierListsRepo.getTierList()
+
+	const tiersWithGames = games.reduce<{name: string; games: TierListGame[]}[]>(
+		(acc, game) => {
+			const tier = acc.find((tier) => tier.name === game.tier)
+			if (tier) {
+				tier.games.push(game)
+			} else {
+				acc[0].games.push(game)
+			}
+			return acc
+		},
+		[
+			{name: '?', games: []},
+			{name: 'S', games: []},
+			{name: 'A', games: []},
+			{name: 'B', games: []},
+			{name: 'C', games: []},
+			{name: 'D', games: []},
+			{name: 'E', games: []},
+			{name: 'F', games: []},
+		],
+	)
 	return {
-		games,
+		tiersWithGames,
 		meta: {
 			title: 'Johnny’s gaming tierlist for 2023!',
 			description:
