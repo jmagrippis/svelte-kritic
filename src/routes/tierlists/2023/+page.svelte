d7aa1517fd160774093397ef12c76ff532802cda
Johnny Magrippis <j@magrippis.com>
2023-12-29 16:28:35 +0100

    ✨  group games to tiers

--- src/routes/tierlists/2023/+page.svelte
+++ src/routes/tierlists/2023/+page.svelte
@@ -5,37 +5,41 @@
 </script>
 
 <main class="content-grid gap-y-4 text-xl lg:gap-y-8 lg:text-2xl">
-	<h1 class="text-4xl font-bold lg:text-6xl">Games of 2023</h1>
+	<h1 class="text-4xl font-bold lg:text-6xl">SvelteKritic Games of 2023</h1>
 	<p>This is a categorisation of the games Johnny’s played in 2023!</p>
 	<p>
 		Not necessarily released in 2023, but <strong>played</strong> for a meaningful
 		amount time in 2023 😄
 	</p>
-	<ul class="breakout games-grid grid grid-flow-dense gap-4">
-		{#each data.games as { id, name, tier, image, startedPlayingAt }}
-			<li
-				id={`${id}`}
-				class="game grid gap-4 rounded bg-surface-brand-2/60 pb-4 transition duration-300 hover:brightness-125 hover:saturate-[75%]"
-			>
-				<a href={`/game/${id}`}>
-					<img
-						src={image}
-						alt={`Box art for ${name}`}
-						class="aspect-game-box rounded-t"
-						width={480}
-						height={720}
-					/>
-				</a>
-				<div class="px-4">
-					<h3 class="font-bold">{name}</h3>
-					<p class="italic">
-						First played: {dateFormat.format(new Date(startedPlayingAt))}
-					</p>
-					<p class="text-2xl">Tier: {tier}</p>
-				</div>
-			</li>
-		{/each}
-	</ul>
+	{#each data.tiersWithGames as tierWithGames}
+		{#if tierWithGames.games.length > 0}
+			<h2 class="text-3xl font-bold">{tierWithGames.name} Tier</h2>
+			<ul class="breakout games-grid grid grid-flow-dense gap-4">
+				{#each tierWithGames.games as { id, name, tier, image, startedPlayingAt }}
+					<li
+						id={`${id}`}
+						class="game grid gap-4 rounded bg-surface-brand-2/60 pb-4 transition duration-300 hover:brightness-125 hover:saturate-[75%]"
+					>
+						<a href={`/game/${id}`}>
+							<img
+								src={image}
+								alt={`Box art for ${name}`}
+								class="aspect-game-box rounded-t"
+								width={480}
+								height={720}
+							/>
+						</a>
+						<div class="px-4">
+							<h3 class="font-bold">{name}</h3>
+							<p class="italic">
+								First played: {dateFormat.format(new Date(startedPlayingAt))}
+							</p>
+						</div>
+					</li>
+				{/each}
+			</ul>
+		{/if}
+	{/each}
 </main>
 
 <style lang="postcss">
